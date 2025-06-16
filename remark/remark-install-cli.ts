9dc3c3b8c813fb3ac5f0dff3306ad556b72e933b
Brendan Dash <me@aiwan.run>
2025-06-16 16:02:57 +0800

    feat: handle source code by remark

--- remark/remark-install-cli.ts
+++ remark/remark-install-cli.ts
@@ -3,6 +3,7 @@ import type { Root } from 'mdast'
 import type { MdxJsxFlowElement } from 'mdast-util-mdx-jsx'
 import type { Transformer } from 'unified'
 
+
 export function remarkInstallCli(): Transformer<Root, Root> {
   const fn = (node: MdxJsxFlowElement) => {
     node.children.forEach((n) => {
--- remark/remark-registry-source-code.ts
+++ remark/remark-registry-source-code.ts
@@ -1,36 +1,37 @@
 import { readFileSync } from 'node:fs'
+import path from 'node:path'
 import { globSync } from 'glob'
 import { visit } from 'unist-util-visit'
 import type { Root } from 'mdast'
 import type { MdxJsxFlowElement } from 'mdast-util-mdx-jsx'
 import type { Transformer } from 'unified'
 
+// eslint-disable-next-line n/prefer-global/process
+const registryPath = path.join(process.cwd(), 'registry')
+
+const fn = (node: MdxJsxFlowElement) => {
+  node.children.forEach((child) => {
+    if (child.type !== 'mdxJsxFlowElement') return
+    fn(child)
+  })
+
+  if (node.name !== 'RegistrySourceCode') return
+  const attributesValue = node.attributes[0].value
+  const files = globSync(`${registryPath}/${attributesValue}.{ts,tsx}`)
+
+  const file = files[0]
+  const ext = file.split('.').pop()
+  const content = readFileSync(file, 'utf-8')
+
+  Object.assign(node, {
+    type: 'code',
+    lang: ext ?? 'ts',
+    meta: `title=\"${attributesValue}.${ext}\"`,
+    value: content,
+  })
+}
+
 export function remarkRegistrySourceCode(): Transformer<Root, Root> {
-  const fn = (node: MdxJsxFlowElement) => {
-    node.children.forEach((n) => {
-      if (n.type !== 'mdxJsxFlowElement') return
-
-      fn(n)
-    })
-
-    if (node.name !== 'RegistrySourceCode') return
-
-    const attrValue = node.attributes[0]?.value
-    const files = globSync(`./registry/**/${attrValue}.{ts,tsx}`)
-    const file = files[0]
-    if (!file) return
-
-    const ext = file.split('.').pop()
-    const value = readFileSync(file, 'utf-8')
-
-    Object.assign(node, {
-      type: 'code',
-      lang: ext,
-      meta: `title=\"${attrValue}.${ext}\"`,
-      value,
-    })
-  }
-
   return (tree) => {
     visit(tree, 'mdxJsxFlowElement', (node) => {
       fn(node)
