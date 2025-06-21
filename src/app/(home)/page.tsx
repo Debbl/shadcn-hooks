e82be5c88ecfdf97aaa24470664ab938757445ab
Brendan Dash <me@aiwan.run>
2025-06-21 22:18:59 +0800

    feat: add external hooks

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,14 +1,12 @@
-import { Button } from '~/components/ui/button'
-
 export default function Page() {
   return (
     <div className='flex flex-1 flex-col items-center justify-center'>
       <h1 className='text-4xl font-bold'>
         A React Hooks Collection built with Shadcn
       </h1>
-      <div className='mt-4 flex flex-col gap-4'>
+      {/* <div className='mt-4 flex flex-col gap-4'>
         <Button>Get Started</Button>
-      </div>
+      </div> */}
     </div>
   )
 }
