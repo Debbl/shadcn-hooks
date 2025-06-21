b11b5028ddbda3904dcdab46a98359e409a0401d
Brendan Dash <me@aiwan.run>
2025-06-21 18:39:45 +0800

    feat: logo

--- src/app/layout.config.tsx
+++ src/app/layout.config.tsx
@@ -5,12 +5,31 @@ export const baseOptions: BaseLayoutProps = {
     title: (
       <>
         <svg
-          width='24'
-          height='24'
           xmlns='http://www.w3.org/2000/svg'
-          aria-label='Logo'
+          width='256'
+          height='256'
+          viewBox='0 0 256 256'
+          fill='none'
+          className='size-4'
         >
-          <circle cx={12} cy={12} r={12} fill='currentColor' />
+          <path
+            d='M208 128L128 208'
+            stroke='currentColor'
+            strokeWidth='32'
+            strokeLinecap='round'
+            strokeLinejoin='round'
+          />
+          <path
+            d='M192 40L40 192'
+            stroke='currentColor'
+            strokeWidth='32'
+            strokeLinecap='round'
+            strokeLinejoin='round'
+          />
+          <path
+            d='M90 65C90 78.8071 78.8071 90 65 90C51.1928 90 40 78.8071 40 65C40 51.1929 51.1928 40 65 40C78.8071 40 90 51.1929 90 65Z'
+            fill='currentColor'
+          />
         </svg>
         Shadcn Hooks
       </>
