465210b67ea145b42fbd1333ddf4cc012ba840c6
Brendan Dash <me@aiwan.run>
2025-06-23 18:41:02 +0800

    chore: update logo

--- src/app/layout.config.tsx
+++ src/app/layout.config.tsx
@@ -9,27 +9,24 @@ export const baseOptions: BaseLayoutProps = {
           width='256'
           height='256'
           viewBox='0 0 256 256'
-          fill='none'
           className='size-4'
+          fill='none'
         >
           <path
             d='M208 128L128 208'
-            stroke='currentColor'
+            stroke='black'
             strokeWidth='32'
             strokeLinecap='round'
             strokeLinejoin='round'
           />
           <path
             d='M192 40L40 192'
-            stroke='currentColor'
+            stroke='black'
             strokeWidth='32'
             strokeLinecap='round'
             strokeLinejoin='round'
           />
-          <path
-            d='M90 65C90 78.8071 78.8071 90 65 90C51.1928 90 40 78.8071 40 65C40 51.1929 51.1928 40 65 40C78.8071 40 90 51.1929 90 65Z'
-            fill='currentColor'
-          />
+          <rect x='27' y='28' width='70' height='70' rx='7' fill='black' />
         </svg>
         Shadcn Hooks
       </>
