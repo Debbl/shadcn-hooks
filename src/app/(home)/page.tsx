363406ba06a19c16d698409e11c7807a58c2ec43
Brendan Dash <me@aiwan.run>
2025-10-17 15:32:13 +0800

    chore: update description

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -2,7 +2,7 @@ export default function Page() {
   return (
     <div className='flex flex-1 flex-col items-center justify-center'>
       <h1 className='text-4xl font-bold'>
-        A React Hooks Collection built with Shadcn
+        A comprehensive React Hooks Collection built with Shadcn
       </h1>
       {/* <div className='mt-4 flex flex-col gap-4'>
         <Button>Get Started</Button>
