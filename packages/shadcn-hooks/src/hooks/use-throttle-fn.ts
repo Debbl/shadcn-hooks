45adf16f43c6945a309b8c818cf9cc414c343f57
Brendan Dash <me@aiwan.run>
2025-11-17 14:05:06 +0800

    feat: upgrade es-toolkit (#19)

--- packages/shadcn-hooks/src/hooks/use-throttle-fn.ts
+++ packages/shadcn-hooks/src/hooks/use-throttle-fn.ts
@@ -2,21 +2,9 @@ import { throttle } from 'es-toolkit'
 import { useMemo } from 'react'
 import { useLatest } from '~/hooks/use-latest'
 import { useUnmount } from '~/hooks/use-unmount'
+import type { ThrottleOptions } from 'es-toolkit'
 
-export interface ThrottleOptions {
-  /**
-   * An optional AbortSignal to cancel the throttled function.
-   */
-  signal?: AbortSignal
-  /**
-   * An optional array specifying whether the function should be invoked on the leading edge, trailing edge, or both.
-   * If `edges` includes "leading", the function will be invoked at the start of the delay period.
-   * If `edges` includes "trailing", the function will be invoked at the end of the delay period.
-   * If both "leading" and "trailing" are included, the function will be invoked at both the start and end of the delay period.
-   * @default ["leading", "trailing"]
-   */
-  edges?: Array<'leading' | 'trailing'>
-}
+export type { ThrottleOptions }
 
 export function useThrottleFn<Fn extends (...args: any[]) => any>(
   fn: Fn,
