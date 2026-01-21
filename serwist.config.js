858acb43aeec7f0872e045da0d2d2194fd3adca1
Brendan Dash <me@aiwan.run>
2026-01-21 14:24:58 +0800

    fix: vercel request limit
    serwist request manifest file

--- serwist.config.js
+++
@@ -1,8 +0,0 @@
-// @ts-check
-import { serwist } from '@serwist/next/config'
-
-export default serwist({
-  swSrc: 'src/app/sw.ts',
-  swDest: 'out/sw.js',
-  globIgnores: ['.next/server/app/_global-error.html', 'public/r/**/*.json'],
-})
--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -2,7 +2,6 @@ import { RootProvider } from 'fumadocs-ui/provider/next'
 import { domMax, LazyMotion } from 'motion/react'
 import { ThemeProvider } from 'next-themes'
 import { NuqsAdapter } from 'nuqs/adapters/next/app'
-import { SerwistProvider } from '~/app/serwist'
 import SearchDialog from '~/components/search'
 
 export interface ProvidersProps {
@@ -11,29 +10,27 @@ export interface ProvidersProps {
 
 export function Providers({ children }: ProvidersProps) {
   return (
-    <SerwistProvider swUrl='/sw.js'>
-      <RootProvider
-        search={{
-          enabled: true,
-          SearchDialog,
-          options: {
-            type: 'static',
-          },
-        }}
+    <RootProvider
+      search={{
+        enabled: true,
+        SearchDialog,
+        options: {
+          type: 'static',
+        },
+      }}
+    >
+      <ThemeProvider
+        attribute='class'
+        defaultTheme='system'
+        enableSystem
+        disableTransitionOnChange
       >
-        <ThemeProvider
-          attribute='class'
-          defaultTheme='system'
-          enableSystem
-          disableTransitionOnChange
-        >
-          <NuqsAdapter>
-            <LazyMotion strict features={domMax}>
-              {children}
-            </LazyMotion>
-          </NuqsAdapter>
-        </ThemeProvider>
-      </RootProvider>
-    </SerwistProvider>
+        <NuqsAdapter>
+          <LazyMotion strict features={domMax}>
+            {children}
+          </LazyMotion>
+        </NuqsAdapter>
+      </ThemeProvider>
+    </RootProvider>
   )
 }
